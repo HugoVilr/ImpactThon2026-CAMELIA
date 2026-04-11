dda611ab0ab3f0beb4918f7b5451bf4e9d3ddf5a
JaviCarb <javier.carballal.morgade@udc.es>
2026-04-11 19:33:26 +0200

    xr first steps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./home";
+import { TestXrPage } from "./test-xr/TestXrPage";
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/test-xr" element={<TestXrPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
